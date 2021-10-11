b3cce7a2ce83c3f23635e2f26bf1b67deb7cbb96
Brian Eyster <eysterbrian@gmail.com>
2021-10-11 13:48:08 -0600

    Adds react-table, types, and basic test component

---
+++ src/components/TestTable.tsx
@@ -0,0 +1,93 @@
+// react-table returns the key prop automatically
+/* eslint-disable react/jsx-key */
+
+import React from 'react';
+import { Table, Thead, Tbody, Tr, Th, Td, chakra } from '@chakra-ui/react';
+import { TriangleDownIcon, TriangleUpIcon } from '@chakra-ui/icons';
+import { useTable, useSortBy, Column } from 'react-table';
+
+export function TestTable() {
+  const data = React.useMemo(
+    () => [
+      {
+        fromUnit: 'inches',
+        toUnit: 'millimetres (mm)',
+        factor: 25.4,
+      },
+      {
+        fromUnit: 'feet',
+        toUnit: 'centimetres (cm)',
+        factor: 30.48,
+      },
+      {
+        fromUnit: 'yards',
+        toUnit: 'metres (m)',
+        factor: 0.91444,
+      },
+    ],
+    []
+  );
+
+  const columns: Array<Column> = React.useMemo(
+    () => [
+      {
+        Header: 'To convert',
+        accessor: 'fromUnit',
+      },
+      {
+        Header: 'Into',
+        accessor: 'toUnit',
+      },
+      {
+        Header: 'Multiply by',
+        accessor: 'factor',
+        isNumeric: true,
+      },
+    ],
+    []
+  );
+
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    useTable({ columns, data }, useSortBy);
+
+  return (
+    <Table {...getTableProps()}>
+      <Thead>
+        {headerGroups.map((headerGroup) => (
+          <Tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map((column) => (
+              <Th
+                {...column.getHeaderProps(column.getSortByToggleProps())}
+                isNumeric={column.isNumeric}>
+                {column.render('Header')}
+                <chakra.span pl="4">
+                  {column.isSorted ? (
+                    column.isSortedDesc ? (
+                      <TriangleDownIcon aria-label="sorted descending" />
+                    ) : (
+                      <TriangleUpIcon aria-label="sorted ascending" />
+                    )
+                  ) : null}
+                </chakra.span>
+              </Th>
+            ))}
+          </Tr>
+        ))}
+      </Thead>
+      <Tbody {...getTableBodyProps()}>
+        {rows.map((row) => {
+          prepareRow(row);
+          return (
+            <Tr {...row.getRowProps()}>
+              {row.cells.map((cell) => (
+                <Td {...cell.getCellProps()} isNumeric={cell.column.isNumeric}>
+                  {cell.render('Cell')}
+                </Td>
+              ))}
+            </Tr>
+          );
+        })}
+      </Tbody>
+    </Table>
+  );
+}
--- src/pages/workouts.tsx
+++ src/pages/workouts.tsx
@@ -1,21 +1,34 @@
 // Displays listing of all workouts for the current user
-
+import React from 'react';
 import fetchWorkouts from 'lib/queries/fetchWorkouts';
 import { useSession } from 'next-auth/client';
 import { useQuery } from 'react-query';
 import Head from 'next/head';
 import { ComponentWithAuth } from 'types/auth';
+import { TestTable } from 'components/TestTable';
 
 const WorkoutsPage: ComponentWithAuth = () => {
-  const [session, loading] = useSession();
-  const { data } = useQuery('workouts', fetchWorkouts);
+  const [session] = useSession();
+  const workoutsQuery = useQuery(
+    ['workouts', session?.user?.id],
+    fetchWorkouts
+  );
+
   return (
     <>
       <Head>
         <title>All workouts</title>
       </Head>
       <h2>Workouts Page!</h2>
-      {JSON.stringify(data, null, 2)}
+
+      {workoutsQuery.isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        <>
+          <TestTable />
+          <pre>{JSON.stringify(workoutsQuery.data, null, 2)}</pre>
+        </>
+      )}
     </>
   );
 };
